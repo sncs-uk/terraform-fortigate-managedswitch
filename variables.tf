70e62ac1b905678930176bada0fd0093784851be
rfranks-securenet <25667080+rfranks-securenet@users.noreply.github.com>
2025-11-24 13:06:40 +0000

    Terraform Fmt

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable config_path {
+variable "config_path" {
   description = "Path to base configuration directory"
   type        = string
 }
 
-variable force_update {
+variable "force_update" {
   description = "Whether or not to force update the ports using a timestamp QS"
   type        = bool
   default     = false
